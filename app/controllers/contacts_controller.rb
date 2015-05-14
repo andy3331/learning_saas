6f8c7391a14b7bcf275cf76fa155cda6ca4614fb
Andrew Connington <aconnington@gmail.com>
2015-05-14 16:04:31 +0000

    created flash hash error/success as well as added forms validation to contact model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,8 +2,32 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+ 
+  #this is called when hitting submit form...
+  #action=/contacts when inspecting element of html page ...as a post
+  #rake routes shows POST   /contacts(.:format)          contacts#create
+  #so we know that we must define the create method in the contacts controller which is below
   def create
+    #create object with (contact_params)
+    #define contact_params in private section below
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      #flash notice displays on screen when put above yield in application.html.erb layout
+      #<div class = "container">
+      #called a flash hash
+      
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
+    else
+      flash[:danger] = "Error occurred, message has not been sent."
+      redirect_to new_contact_path
+    end
   end
 
+  private
+  #white listing the contact form parameters
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-  
+  #validate content is typed in name, email on form. otherwise triggers error "flash hash"
+  validates :name, presence: true
+  validates :email, presence: true
 end
