52b21502c4ec2f32b5d637e55024a68f85aaf718
Andrew Connington <aconnington@gmail.com>
2015-05-18 23:55:44 +0000

    added plan variables to pages controller, used variables on home page as part of path helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def home
+    #creating instance variables for home page based on IDs in Plan table
+    #will be able to use variable to reference the plans on the home page like we did:   (plan: @basic_plan.id) in:
+    #<%= link_to "Sign up with Basic", new_user_registration_path(plan: @basic_plan.id), class: 'btn btn-primary btn-lg btn-block' %>
+    #this causes query string parameters in the url 1 for basic 2 for pro. (ex: http://supersonic-ghost-77-219858.use1.nitrousbox.com/users/sign_up?plan=2)
+    #this lets us configure the sign up form based on plan passed
+    #Plan.all in rails console shows 1 is basic 2 is pro
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   def about
   end
